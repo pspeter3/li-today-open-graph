b3478a964619d7de11cff38724b77368081f9c3c
Phips Peter <pspeter333@gmail.com>
2012-03-14 13:16:04 -0700

    Changing the javascript

--- li-today-open-graph.js
+++ li-today-open-graph.js
@@ -2,8 +2,7 @@
 (function(d, w) {
 	// Adds a script
 
-
-	function addScript(id, src, callback) {
+	function addScript(id, src) {
 		// Prevent the script from being loaded twice
 		if (d.getElementById(id)) {
 			return;
@@ -11,16 +10,8 @@
 		// Sets up the script
 		var js = d.createElement('script');
 		js.id = id;
-		js.async = true;
 		js.src = src;
 		var loaded = false;
-		// Calls the callback when the script is loaded
-		js.onload = js.onreadystatechange = function() {
-			if (!loaded && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-				loaded = true;
-				callback();
-			}
-		}
 		d.getElementsByTagName('head')[0].appendChild(js);
 	}
 
@@ -37,14 +28,7 @@
 		});
 	};
 
-	// Sets up jQuery
-	addScript('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js', function() {
-		jQuery('#extra').append(
-			"<div id=\"fb-root\"></div>
+	jQuery('#extra').append("<div id=\"fb-root\"></div>
 			<fb:login-button show-faces=\"true\" width=\"200\" max-rows=\"1\" scope=\"publish_actions\"></fb:login-button>
-		");
-		addScript('facebook-jssdk', '//connect.facebook.net/en_US/all.js', function() {
-			return;
-		})
-	});
+	");
 }(document, window));
