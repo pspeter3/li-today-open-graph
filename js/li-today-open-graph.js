6b3bdf4358c330c19b2c686312e55fe17a41d2cb
Phips Peter <pspeter333@gmail.com>
2012-03-14 11:08:54 -0700

    Added jQuery and Facebook SDK

--- js/li-today-open-graph.js
+++ js/li-today-open-graph.js
@@ -3,6 +3,7 @@ var LITodayOpenGraph = (function() {
 	// Create the object to represent the module
 	var obj = {};
 
+	// Adds a script async to the page
 	obj.addScript = function(src, id, d) {
 		var js, id = id;
 		if (d.getElementById(id)) {
@@ -17,3 +18,8 @@ var LITodayOpenGraph = (function() {
 
 	return obj;
 }());
+
+// Add the Facebook SDK
+LITodayOpenGraph.addScript('//connect.facebook.net/en_US/all.js', 'facebook-jssdk', document);
+// Add jQuery
+LITodayOpenGraph.addScript('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', 'jquery', document);
