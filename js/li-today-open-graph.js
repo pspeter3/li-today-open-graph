d9494e2d9edfc589b8a95e4b964e52da20fb7203
Phips Peter <pspeter333@gmail.com>
2012-03-14 11:07:01 -0700

    Added the ability to add scripts to the page asyncrounously

--- js/li-today-open-graph.js
+++ js/li-today-open-graph.js
@@ -1,4 +1,19 @@
+// Create the LinkedIn Today Open Graph module
 var LITodayOpenGraph = (function() {
+	// Create the object to represent the module
 	var obj = {};
-	
+
+	obj.addScript = function(src, id, d) {
+		var js, id = id;
+		if (d.getElementById(id)) {
+			return;
+		}
+		js = d.createElement('script');
+		js.id = id;
+		js.async = true;
+		js.src = src;
+		d.getElementsByTagName('head')[0].appendChild(js);
+	}
+
+	return obj;
 }());
