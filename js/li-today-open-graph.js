b46983b57dbd6a49b9fde73dc9f2850c4e9570ab
Phips Peter <pspeter333@gmail.com>
2012-03-14 11:55:37 -0700

    Trying a different approach

---
+++ js/li-today-open-graph.js
@@ -0,0 +1,40 @@
+// Wrap the module and pass in the document and window objects
+(function(d, w) {
+	// Adds a script
+	function addScript(id, src, callback) {
+		// Prevent the script from being loaded twice
+		if (d.getElementById(id)) {
+			return;
+		}
+		// Sets up the script
+		var js = d.createElement('script');
+		js.id = id;
+		js.async = true;
+		js.src = src;
+		var loaded = false;
+		// Calls the callback when the script is loaded
+		js.onload = js.onreadystatechange = function() {
+			if (!loaded && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+				loaded = true;
+				callback();
+			}
+		}
+		d.getElementsByTagName('head')[0].appendChild(js);
+	}
+
+	// Sets up the Facebook async callback
+	w.fbAsyncInit = function() {
+      FB.init({
+        appId      : '[YOUR_APP_ID]', // App ID
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+      });
+    };
+
+	// Sets up jQuery
+	addScript('jquery', '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js', function() {
+		jQuery('#extra').append('<fb:login-button show-faces="true" width="200" max-rows="1" scope="publish_actions"></fb:login-button>')
+		addScript('facebook-jssdk', '//connect.facebook.net/en_US/all.js', function() {return;})
+	});
+}(document, window));
