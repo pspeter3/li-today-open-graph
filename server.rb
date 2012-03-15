604d3c6a035cf556386e18bbd330a36521250140
Phips Peter <pspeter333@gmail.com>
2012-03-14 17:45:53 -0700

    Trying this out

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ get '/' do
 <html>
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# com-linkedin-today-p: http://ogp.me/ns/fb/com-linkedin-today-p#">
 <meta property="fb:app_id"      content="318731064857073" /> 
-<meta property="og:type"        content="com-linkedin-today-p:story" /> 
+<meta property="og:type"        content="article" /> 
 <meta property="og:url"         content="#{CGI::unescape params[:url]}" /> 
 <meta property="og:title"       content="#{CGI::unescape params[:title]}" /> 
 <meta property="og:description" content="#{CGI::unescape params[:description]}" /> 
