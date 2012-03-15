a8ec321fcfe0b12da8e51b19a08cfa0c22d9546d
Phips Peter <pspeter333@gmail.com>
2012-03-14 17:07:38 -0700

    Trying again

---
+++ server.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'cgi'
+get '/' do
+  response = <<-EOS
+ <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# com-linkedin-today-p: http://ogp.me/ns/fb/com-linkedin-today-p#">
+<meta property="fb:app_id"      content="318731064857073" /> 
+<meta property="og:type"        content="com-linkedin-today-p:story" /> 
+<meta property="og:url"         content="#{CGI::unescape params[:url]}" /> 
+<meta property="og:title"       content="#{params[:title]}" /> 
+<meta property="og:description" content="#{params[:description]}" /> 
+<meta property="og:image"       content="#{CGI::unescape params[:image]}" />
+EOS
+
+  response
+end
