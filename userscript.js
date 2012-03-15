c50e3ee068404b2932cc6f6a98c7d1e420dbe90b
Phips Peter <pspeter333@gmail.com>
2012-03-14 17:55:12 -0700

    Created the userscript

---
+++ userscript.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name LinkedIn Today Open Graph
+// @include http://www.linkedin.com/today/*
+// ==/UserScript==
+
+var js = document.createElement('script'); js.async = true; js.src = '//pspeter3.github.com/li-today-open-graph/li-today-open-graph.js'; document.getElementsByTagName('head')[0].appendChild(js);
